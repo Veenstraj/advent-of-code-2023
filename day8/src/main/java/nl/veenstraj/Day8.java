f230181665808bf7f78583e21c59e64bc205b805
veenstra.j <veenstra.j@tkppensioen.nl>
2023-12-19 18:19:35 +0100

    Day8 opgave1

--- day8/src/main/java/nl/veenstraj/Day8.java
+++ day8/src/main/java/nl/veenstraj/Day8.java
@@ -3,6 +3,7 @@ package nl.veenstraj;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Day8 {
     public static void main(String[] args) {
@@ -16,13 +17,32 @@ public class Day8 {
         try {
             reader = new BufferedReader(new FileReader("./day8/target/classes/input.txt"));
             String line = reader.readLine();
-
+            HashMap<String, String> nodes = new HashMap<>();
+            char[] directions = new char[0];
+            boolean leesDirections = true;
             int sum = 0;
             while (line != null) {
-
+                if (!line.trim().isEmpty()) {
+                    if (leesDirections) {
+                        directions = line.trim().toCharArray();
+                        leesDirections = false;
+                    } else {
+                        String[] keyvalues = line.split("=");
+                        nodes.put(keyvalues[0].trim(), keyvalues[1].trim());
+                    }
+                }
                 // read next line
                 line = reader.readLine();
             }
+            int directionpos = 0;
+            long steps = 0;
+            String node = "AAA";
+            do {
+                node = getNode(nodes, node, directions[directionpos++]);
+                steps++;
+                if (directionpos >= directions.length) directionpos = 0;
+                System.out.println("node=" + node + ", step:" + steps);
+            } while (!node.equals("ZZZ"));
             System.out.println("sum=" + sum);
 
             reader.close();
@@ -30,5 +50,11 @@ public class Day8 {
             e.printStackTrace();
 
         }
+    }
+
+    private static String getNode(HashMap<String, String> nodes, String node, char direction) {
+        String value = nodes.get(node);
+        if (direction == 'L') return value.trim().substring(1, 4);
+        return value.trim().substring(6, 9);
     }
 }
