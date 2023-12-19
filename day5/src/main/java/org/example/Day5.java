d45bbfb6b26dc6f142233cb232be768f362a435a
JosVeenstra <veenstra.j@tkppensioen.nl>
2023-12-19 12:19:28 +0100

    Voorbereiden day 7 + 8

--- day5/src/main/java/org/example/Day5.java
+++ day5/src/main/java/org/example/Day5.java
@@ -3,7 +3,11 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.lang.Long.MAX_VALUE;
 import static java.lang.Long.parseLong;
