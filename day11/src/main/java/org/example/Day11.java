45c4769abf8b109a55010a5eda775dbbc1336f38
veenstra.j <veenstra.j@tkppensioen.nl>
2023-12-27 12:16:44 +0100

    Day11 final task 2

--- day11/src/main/java/org/example/Day11.java
+++ day11/src/main/java/org/example/Day11.java
@@ -7,11 +7,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Day11 {
-    private static final char[][] _map = new char[300][300];
-    private static char[][] _visited;
+    private static final String inputfile = "./day11/target/classes/input.txt";
+    private static final int _expansion = 999999;
+    private static final char[][] _map = new char[150][150];
+    private static final int[] _expandedY = new int[15];
+    private static final int[] _expandedX = new int[15];
     private static int _maxx;
     private static int _maxy;
-    private static Map<Integer, Galaxy> _galaxies = new HashMap<>();
+    private static final Map<Integer, Galaxy> _galaxies = new HashMap<>();
 
 
     public static void main(String[] args) {
@@ -22,33 +25,32 @@ public class Day11 {
 
     public static void opgave1() {
         BufferedReader reader;
-        char[][] mapTemp = new char[300][300];
 
         try {
-            reader = new BufferedReader(new FileReader("./day11/target/classes/input.txt"));
+            reader = new BufferedReader(new FileReader(inputfile));
             String line = reader.readLine();
 
             int index = 0;
+            int expansion = 0;
             while (line != null) {
                 if (line.trim().length() > 0) {
                     char[] thisLine = line.trim().toCharArray();
-                    mapTemp[index++] = thisLine;
-                    if (!hasGalaxies(thisLine)) mapTemp[index++] = thisLine;  // expand space row
+                    if (!hasGalaxies(thisLine)) _expandedY[expansion++] = index;
+                    _map[index++] = thisLine;
                 }
                 // read next line
                 line = reader.readLine();
             }
             reader.close();
 
-            // copy and expand
-            copyAndExpand(mapTemp, index);
-            printSpace();
+            _maxy = index;
+            _maxx = _map[0].length;
+            lookForExpansionX();
+            System.out.println("maxx=" + _maxx + ", maxy=" + _maxy);
             System.out.println("Nr of Galaxies=" + findGalaxies());
 
             long sumShortestPath = sumShortestPaths();
             System.out.println("Sum = " + sumShortestPath);
-
-            //_visited = new char[index][_map[0].length];
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,23 +63,19 @@ public class Day11 {
         return false;
     }
 
-    private static void copyAndExpand(char[][] mapTemp, int maxy) {
-        int newx = 0;
-        for (int x = 0; x < mapTemp[0].length; x++, newx++) {
+    private static void lookForExpansionX() {
+        int expansion = 0;
+        int x = 0;
+        for (; x < _maxx; x++) {
             boolean hasGalaxies = false;
-            for (int y = 0; y < maxy; y++) {
-                if (mapTemp[y][x] == '#') hasGalaxies = true;
-                _map[y][newx] = mapTemp[y][x];
-            }
-            if (!hasGalaxies) {
-                newx++;  // expand space column
-                for (int y = 0; y < maxy; y++) {
-                    _map[y][newx] = mapTemp[y][x];
+            for (int y = 0; y < _maxy; y++) {
+                if (_map[y][x] == '#') {
+                    hasGalaxies = true;
+                    break;
                 }
             }
+            if (!hasGalaxies) _expandedX[expansion++] = x;
         }
-        _maxx = newx;
-        _maxy = maxy;
     }
 
     private static int findGalaxies() {
@@ -85,12 +83,30 @@ public class Day11 {
         for (int y = 0; y < _maxy; y++) {
             for (int x = 0; x < _maxx; x++) {
                 if (_map[y][x] == '#') {
-                    _galaxies.put(nrOfGalaxy, new Galaxy(nrOfGalaxy, x, y));
+                    _galaxies.put(nrOfGalaxy, new Galaxy(nrOfGalaxy, getVirtualX(x), getVirtualY(y)));
                     nrOfGalaxy++;
                 }
             }
         }
         return nrOfGalaxy;
+    }
+
+    private static long getVirtualX(int x) {
+        long newx = x;
+        for (int ex = 0; _expandedX[ex] > 0; ex++) {
+            if (x < _expandedX[ex]) return newx;
+            newx += _expansion;
+        }
+        return newx;
+    }
+
+    private static long getVirtualY(int y) {
+        long newy = y;
+        for (int ex = 0; _expandedY[ex] > 0; ex++) {
+            if (y < _expandedY[ex]) return newy;
+            newy += _expansion;
+        }
+        return newy;
     }
 
     private static long sumShortestPaths() {
@@ -101,34 +117,25 @@ public class Day11 {
                 Galaxy galaxySrc = _galaxies.get(galaxynrSrc);
                 Galaxy galaxyDest = _galaxies.get(galaxynrDest);
                 long distance = calcShortestPath(galaxySrc.x, galaxySrc.y, galaxyDest.x, galaxyDest.y, 0);
-                System.out.println(String.format("%02d: Galaxy %d (%d,%d) to %d (%d,%d) is %d", nrOfDistances++,
+                System.out.printf("%02d: Galaxy %d (%d,%d) to %d (%d,%d) is %d%n", nrOfDistances++,
                         galaxySrc.number, galaxySrc.y + 1,
-                        galaxySrc.x + 1, galaxyDest.number, galaxyDest.y + 1, galaxyDest.x + 1, distance));
+                        galaxySrc.x + 1, galaxyDest.number, galaxyDest.y + 1, galaxyDest.x + 1, distance);
                 sum += distance;
             }
         }
         return sum;
     }
 
-    private static long calcShortestPath(int x, int y, int destx, int desty, long distance) {
+    private static long calcShortestPath(long x, long y, long destx, long desty, long distance) {
         return Math.abs(desty - y) + Math.abs(destx - x);
-    }
-
-    private static void printSpace() {
-        for (int y = 0; y < _maxy; y++) {
-            for (int x = 0; x < _maxx; x++) {
-                System.out.printf(String.valueOf(_map[y][x]));
-            }
-            System.out.println();
-        }
     }
 
     static class Galaxy {
         int number;
-        int x;
-        int y;
+        long x;
+        long y;
 
-        public Galaxy(int number, int x, int y) {
+        public Galaxy(int number, long x, long y) {
             this.number = number;
             this.x = x;
             this.y = y;
