5cdd62c9e9ce0fa8ebb182f00acaa6370097d9d7
veenstra.j <veenstra.j@tkppensioen.nl>
2023-12-28 11:02:24 +0100

    Day12 task 2 (brute force)

--- day12/src/main/java/org/example/Day12.java
+++ day12/src/main/java/org/example/Day12.java
@@ -3,6 +3,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 
 public class Day12 {
@@ -20,19 +22,30 @@ public class Day12 {
         try {
             reader = new BufferedReader(new FileReader(inputfile));
             String line = reader.readLine();
+            int[] groups;
             int sum = 0;
             while (line != null) {
                 if (line.trim().length() > 0) {
                     String conditionRecord = line.trim().split(" ")[0].trim();
                     String checksums = line.trim().split(" ")[1].trim();
 
+                    // part 2: Extend with 5 copies of itself, separated by '?'
+                    conditionRecord = conditionRecord + "?" + conditionRecord + "?" + conditionRecord + "?" + conditionRecord + "?" + conditionRecord;
+                    checksums = checksums + "," + checksums + "," + checksums + "," + checksums + "," + checksums;
+
+                    groups = Arrays.stream(checksums.split(",")).mapToInt(Integer::parseInt).toArray();
                     System.out.println("check " + conditionRecord);
                     String condRecordToCheck = conditionRecord.replaceAll("\\?", "d");
                     char[] arrayRecordTocheck = condRecordToCheck.toCharArray();
+                    int sequence = 0;
                     do {
                         arrayRecordTocheck = getConditionrecord(arrayRecordTocheck, 0);
-                        //System.out.print("Check " + String.valueOf(arrayRecordTocheck).replaceAll("d",".").replaceAll("h", "#"));
-                        if (checkGroups(arrayRecordTocheck, checksums)) sum++;
+                        System.out.print("\rCheck " + String.valueOf(arrayRecordTocheck));//.replaceAll("d",".").replaceAll("h", "#"));
+                        //System.out.println("\r" + sequence++);
+                        if (checkGroups(arrayRecordTocheck, groups)) {
+                            sum++;
+                            //System.out.println("sum=" + sum);
+                        }
                     } while (!condRecordToCheck.equals(String.valueOf(arrayRecordTocheck)));
                 }
                 // read next line
@@ -55,47 +68,40 @@ public class Day12 {
                     rec[pos] = 'h'; // h=hash
                     return rec;
 
-                } else {
-                    if (rec[pos] == 'h') {
-                        rec[pos] = 'd';
-                    }
                 }
+                if (rec[pos] == 'h') {
+                    rec[pos] = 'd';
+                }
+
             }
             return getConditionrecord(rec, pos + 1);
         }
         return rec;
     }
 
-    private static boolean checkGroups(char[] arrayTocheck, String checksums) {
+    private static boolean checkGroups(char[] arrayTocheck, int[] groups) {
         int group = 0;
-        StringBuilder foundGroups = new StringBuilder();
+        int[] foundGroups = new int[100];
+        int groupindex = 0;
         for (char c : arrayTocheck) {
             if (c == '#' || c == 'h') {
                 group++;
             } else {
                 if (group > 0) {
-                    if (foundGroups.isEmpty()) {
-                        foundGroups.append(group);
-                    } else {
-                        foundGroups.append(",").append(group);
+                    if (groupindex >= groups.length || group != groups[groupindex]) {
+                        return false;
                     }
+                    foundGroups[groupindex++] = group;
                 }
                 group = 0;
             }
         }
         if (group > 0) {
-            if (foundGroups.isEmpty()) {
-                foundGroups.append(group);
-            } else {
-                foundGroups.append(",").append(group);
-            }
+            foundGroups[groupindex++] = group;
         }
-        if (foundGroups.toString().equals(checksums)) {
-            // System.out.println(" > Match!");
-            return true;
-        } else {
-            // System.out.println(" " + foundGroups.toString() + "/" + checksums);
+        if (groupindex != groups.length) {
             return false;
         }
+        return IntStream.range(0, groupindex).noneMatch(p -> foundGroups[p] != groups[p]);
     }
 }
