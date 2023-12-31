2f5c3fc03feac2b37c820a13e544e73ca5451077
veenstra.j <veenstra.j@tkppensioen.nl>
2023-12-31 12:05:18 +0100

    Day13 task 2 not working

--- day13/src/main/java/org/example/Day13.java
+++ day13/src/main/java/org/example/Day13.java
@@ -3,10 +3,9 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Day13 {
-    private static final String inputfile = "./day13/target/classes/input.txt";
+    private static final String inputfile = "./day13/target/classes/inputTst.txt";
     private static char[][] _map = new char[20][20];
     private static int _sizex = 0;
     private static int _sizey = 0;
@@ -32,8 +31,8 @@ public class Day13 {
                     System.out.println("New map...");
                     // end of pattern, do the magic
                     _sizex = _map[0].length;
-                    sum += findVerticalReflection();
-                    sum += 100 * findHorizontalReflection();
+                    sum += findVerticalReflection(true);
+                    sum += 100 * findHorizontalReflection(true);
 
                     // new pattern
                     _sizey = 0;
@@ -54,21 +53,25 @@ public class Day13 {
         }
     }
 
-    private static int findVerticalReflection() {
-        boolean[] reflection = new boolean[_sizex - 1];
-        Arrays.fill(reflection, true);
+    private static int findVerticalReflection(boolean findSmudge) {
+        System.out.printf("sizex=%d, sizey=%d, findsmudge=%b%n", _sizex, _sizey, findSmudge);
+
+        int[] reflection = new int[_sizex - 1];
+        int[] reflectiony = new int[_sizex - 1];
         for (int y = 0; y < _sizey; y++) {
             for (int x = 0; x < _sizex - 1; x++) {
-                if (reflection[x]) {
+                if (reflection[x] == 0) {
                     for (int dx = 0; dx <= x && (x + dx + 1) < _sizex; dx++) {
-                        System.out.printf("x checking (%d,%d)=%c to (%d,%d)=%c: ",
-                                y + 1, x - dx + 1, _map[y][x - dx], y + 1, x + dx + 2, _map[y][x + dx + 1]);
-                        if (_map[y][x - dx] != _map[y][x + dx + 1]) {
-                            reflection[x] = false;
-                            System.out.printf("Reflection %d = %b (set)%n", x + 1, reflection[x]);
-                            break;
+                        System.out.printf("X checking %s %c to %s %c: ",
+                                prtCoord(x - dx, y), get(x - dx, y), prtCoord(x + dx + 1, y), get(x + dx + 1, y));
+                        if (get(x - dx, y) != get(x + dx + 1, y)) {
+                            reflection[x]++;
+                            reflectiony[x] = y;
+                            System.out.printf("Reflection %d = %d (set)%n", x + 1, reflection[x]);
+                            if (reflection[x] > 2) break;
+                        } else {
+                            System.out.printf("Reflection %d = %d%n", x + 1, reflection[x]);
                         }
-                        System.out.printf("Reflection %d = %b%n", x + 1, reflection[x]);
                     }
                 }
             }
@@ -76,29 +79,54 @@ public class Day13 {
 
         int answer = 0;
         for (int x = 0; x < _sizex - 1; x++) {
-            if (reflection[x]) {
-                System.out.println("reflection at x=" + (x + 1));
+            System.out.println("reflection at x=" + (x + 1) + " is " + reflection[x]);
+            if (reflection[x] == 1 && findSmudge) {
+                System.out.printf("Smudge found at %s%n", prtCoord(x, reflectiony[x]));
+                // repair smudge
+                set(x, reflectiony[x], get(x, reflectiony[x]) == '.' ? '#' : '.');
+                int possibleRefection = findVerticalReflection(false); //again, with repaired mirror
+                if (possibleRefection == 0 || possibleRefection == (reflection[x] + 1)) {
+                    System.out.println("no new reflection (" + possibleRefection + ")");
+                    possibleRefection = findHorizontalReflection(false);
+                    if (possibleRefection == 0) {
+                        System.out.println("no succes,restore smudge and continue with next possibility");
+                        set(x, reflectiony[x], get(x, reflectiony[x]) == '.' ? '#' : '.');
+                    } else {
+                        System.out.println("Found horizontal reflection");
+                        return 0;
+                    }
+                } else {
+                    return possibleRefection + 1;
+                }
+            }
+            if (!findSmudge && reflection[x] == 0) {
+                System.out.println("answer=" + (x + 1));
                 answer += x + 1;
+                return answer;
             }
         }
         return answer;
     }
 
-    private static int findHorizontalReflection() {
-        boolean[] reflection = new boolean[_sizey - 1];
-        Arrays.fill(reflection, true);
+    private static int findHorizontalReflection(boolean findSmudge) {
+        System.out.printf("sizex=%d, sizey=%d, findsmudge=%b%n", _sizex, _sizey, findSmudge);
+
+        int[] reflection = new int[_sizey - 1];
+        int[] reflectionx = new int[_sizey - 1];
         for (int x = 0; x < _sizex; x++) {
             for (int y = 0; y < _sizey - 1; y++) {
-                if (reflection[y]) {
+                if (reflection[y] == 0) {
                     for (int dy = 0; dy <= y && (y + dy + 1) < _sizey; dy++) {
-                        System.out.printf("checking (%d,%d)=%c to (%d,%d)=%c: ",
-                                y - dy + 1, x + 1, _map[y - dy][x], y + dy + 2, x + 1, _map[y + dy + 2][x]);
-                        if (_map[y - dy][x] != _map[y + dy + 1][x]) {
-                            reflection[y] = false;
-                            System.out.printf("Reflection %d = %b (set)%n", y + 1, reflection[y]);
-                            break;
+                        System.out.printf("Y checking %s %c to %s %c: ",
+                                prtCoord(x, y - dy), get(x, y - dy), prtCoord(x, y + dy + 1), get(x, y + dy + 1));
+                        if (get(x, y - dy) != get(x, y + dy + 1)) {
+                            reflection[y]++;
+                            reflectionx[y] = x;
+                            System.out.printf("Reflection %d = %d (set)%n", y + 1, reflection[y]);
+                            if (reflection[y] > 2) break;
+                        } else {
+                            System.out.printf("Reflection %d = %d%n", y + 1, reflection[y]);
                         }
-                        System.out.printf("Reflection %d = %b%n", y + 1, reflection[y]);
                     }
                 }
             }
@@ -106,13 +134,32 @@ public class Day13 {
 
         int answer = 0;
         for (int y = 0; y < _sizey - 1; y++) {
-            if (reflection[y]) {
-                System.out.println("reflection at y=" + (y + 1));
+            System.out.println("reflection at y=" + (y + 1) + " is " + reflection[y]);
+            if (reflection[y] == 1 && findSmudge) {
+                System.out.printf("Smudge found at %s%n", prtCoord(reflectionx[y], y));
+//                // repair smudge
+//                set(reflectionx[y], y, get(reflectionx[y],y) == '.' ? '#': '.');
+//                return findHorizontalReflection(false); //again, with repaired mirror
+            }
+            if (reflection[y] == 0) {
+                System.out.println("answer=" + (y + 1));
                 answer += y + 1;
             }
 
         }
         return answer;
 
+    }
+
+    private static char get(int x, int y) {
+        return _map[y][x];
+    }
+
+    private static void set(int x, int y, char a) {
+        _map[y][x] = a;
+    }
+
+    private static String prtCoord(int x, int y) {
+        return String.format("(%d,%d)", y + 1, x + 1);
     }
 }
