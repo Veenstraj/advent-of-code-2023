74b9530f75b93dfc0d0158e7d88ed66e63c3b926
veenstra.j <veenstra.j@tkppensioen.nl>
2023-12-31 17:47:29 +0100

    Day15 task 2 final

--- day15/src/main/java/org/example/Day15.java
+++ day15/src/main/java/org/example/Day15.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 public class Day15 {
     private static final String inputfile = "./day15/target/classes/input.txt";
 
+    private static String[][] _box = new String[256][1000];
+
     public static void main(String[] args) {
 
         opgave1();
@@ -21,18 +23,29 @@ public class Day15 {
             String line = reader.readLine();
 
             int sum = 0;
-            int hash = 0;
             while (line != null) {
                 if (line.trim().length() > 0) {
                     String[] subject = line.trim().split(",");
                     for (String value : subject) {
-                        hash = 0;
-                        for (int i = 0; i < value.length(); i++) {
-                            hash += (int) value.charAt(i);
-                            hash = (hash * 17) % 256;
+                        String label = value.split("[=-]")[0];
+                        String operation = value.substring(label.length(), label.length() + 1);
+                        String focalLength = "";
+                        if (operation.equals("=")) {
+                            focalLength = value.substring(label.length() + 1);
                         }
-                        sum += hash;
+                        int boxnr = 0;
+                        for (int i = 0; i < label.length(); i++) {
+                            boxnr = hash(boxnr, label.charAt(i));
+                        }
+                        if (operation.equals("=")) {
+                            storeInBox(boxnr, label, focalLength);
+                        }
+                        if (operation.equals("-")) {
+                            removeFromBox(boxnr, label);
+                        }
+                        //printBoxes(value);
                     }
+                    sum = calcSum();
                 }
 
                 // read next line
@@ -41,11 +54,67 @@ public class Day15 {
             reader.close();
 
 
-            System.out.println("sum=" + sum);
+            System.out.println("\nsum=" + sum);
 
         } catch (IOException e) {
             e.printStackTrace();
 
         }
+    }
+
+    private static int hash(int hash, char input) {
+        hash += (int) input;
+        return (hash * 17) % 256;
+    }
+
+    private static void storeInBox(int boxnr, String label, String focalLength) {
+        int x = 0;
+        for (; _box[boxnr][x] != null; x++) {
+            if (_box[boxnr][x].startsWith(label)) {
+                break;
+            }
+        }
+        _box[boxnr][x] = label + " " + focalLength;
+    }
+
+    private static void removeFromBox(int boxnr, String label) {
+        for (int x = 0; _box[boxnr][x] != null; x++) {
+            if (_box[boxnr][x].startsWith(label)) {
+                int x1 = x;
+                for (; _box[boxnr][x1 + 1] != null; x1++) {
+                    _box[boxnr][x1] = _box[boxnr][x1 + 1];
+                }
+                _box[boxnr][x1] = null;
+                break;
+            }
+        }
+    }
+
+    private static int calcSum() {
+        int sum = 0;
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (_box[i][j] != null) {
+                    int focalpos = _box[i][j].indexOf(" ");
+
+                    System.out.println("sum+=" + (i + 1) + "*" + (j + 1) + "*" + Integer.parseInt(_box[i][j].substring(focalpos + 1)));
+                    sum += (i + 1) * (j + 1) * Integer.parseInt(_box[i][j].substring(focalpos + 1));
+                }
+            }
+        }
+        return sum;
+    }
+
+    private static void printBoxes(String value) {
+        System.out.println("\n\nAfter " + value + ":");
+        for (int i = 0; i < 256; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (_box[i][j] != null) {
+                    if (j == 0) System.out.print("\nBox " + i + ":");
+                    System.out.printf(" [" + _box[i][j] + "]");
+                }
+            }
+        }
+
     }
 }
